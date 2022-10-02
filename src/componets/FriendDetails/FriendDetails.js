3a9751d0f33d0c5b48eb63ab358cf8557b65d42d
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-02 13:48:58 +0600

    add FriendDetails components

---
+++ src/componets/FriendDetails/FriendDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetails = () => {
+    return (
+        <div>
+            <h2>Everything you need to know about this persona</h2>
+        </div>
+    );
+};
+
+export default FriendDetails;
